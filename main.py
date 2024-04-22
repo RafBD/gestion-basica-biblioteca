0adf18050181fd1bafc0fb4e6a9612579771d0e3
RaffaBD <rafa.bediaz@gmail.com>
2024-04-22 15:28:53 -0600

    Removed unnecessary tkinter import

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import messagebox
 from db_connection import connect_db
 from tkinter_ui import LibraryGUI
 
@@ -9,6 +8,6 @@ db_connection = connect_db()
 # Iniciar la interfaz grafica
 if db_connection:
     library_gui = LibraryGUI(db_connection)
-    library_gui.window.mainloop()
+    library_gui.start()
 else:
     messagebox.showerror("Error", "No se pudo conectar a la base de datos")
