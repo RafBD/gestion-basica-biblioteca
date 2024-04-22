3d43af08a9e70ce84db6e30a4dd959f15ab3d53d
RaffaBD <rafa.bediaz@gmail.com>
2024-04-22 17:26:33 -0600

    Fixed DB connection

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from tkinter import messagebox
 from db_connection import connect_db
+from tkinter import messagebox
 from tkinter_ui import LibraryGUI
 
-# Conectar a la base de datos
-db_connection = connect_db()
-
 # Iniciar la interfaz grafica
-if db_connection:
-    library_gui = LibraryGUI(db_connection)
-    library_gui.start()
-else:
-    messagebox.showerror("Error", "No se pudo conectar a la base de datos")
+if __name__ == "__main__":
+    # Conectar a la base de datos
+    db_connection = connect_db()
+
+    if db_connection:
+        library_gui = LibraryGUI(db_connection)
+        library_gui.window.mainloop()
+    else:
+        messagebox.showerror("Error", "Error al conectar con la base de datos")
